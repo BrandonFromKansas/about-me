6440ab4899c81fcdce1642377dd189443762cd27
Brandon Jensen <brandon.l.jensen@gmail.com>
2024-02-07 18:55:27 -0800

    actually adding my js folder and sub-files.

---
+++ js/javascript-lab06.js
@@ -0,0 +1,7 @@
+let color = prompt("What color would you paint your dog?");
+
+document.write("Heck yeah, " + color + " is an awesome color!\n");
+
+let fish = prompt("How many fish did you catch?");
+
+document.write("Damn, " + fish + " is a lot of fish!");
